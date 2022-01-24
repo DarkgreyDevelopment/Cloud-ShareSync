bda6409879fd8ae3f1522e1b37d218235c6b6611
Taylor Marvin <Taylor@Marvin.email>
2022-01-23 16:20:09 -0800

    Reduce logging to (hopefully) speed up app startup.

--- src/SimpleBackup/BackupProcess/DatabaseUtility/SqliteContext.cs
+++ src/SimpleBackup/BackupProcess/DatabaseUtility/SqliteContext.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using Cloud_ShareSync.Core.Database.Sqlite;
+﻿using Cloud_ShareSync.Core.Database.Sqlite;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cloud_ShareSync.SimpleBackup {
@@ -7,21 +6,13 @@ namespace Cloud_ShareSync.SimpleBackup {
     public partial class Program {
 
         private static SqliteContext GetSqliteContext( ) {
-            using Activity? activity = s_source.StartActivity( "GetSqliteContext" )?.Start( );
             if (s_services == null) { throw new InvalidOperationException( "Cannot get context if db service is not initialized." ); }
-            s_logger?.ILog?.Debug( "Waiting for SqliteContext Semaphore" );
             s_semaphore.Wait( );
             SqliteContext result = s_services.Services.GetRequiredService<SqliteContext>( );
-            activity?.Stop( );
             return result;
         }
 
-        private static void ReleaseSqliteContext( ) {
-            using Activity? activity = s_source.StartActivity( "ReleaseSqliteContext" )?.Start( );
-            s_logger?.ILog?.Debug( "Releasing SqliteContext Semaphore" );
-            s_semaphore.Release( );
-            activity?.Stop( );
-        }
+        private static void ReleaseSqliteContext( ) { s_semaphore.Release( ); }
 
     }
 }
