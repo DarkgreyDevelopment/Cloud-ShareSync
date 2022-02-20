0d50d79ab8baa8b8e12198bb6b64c8d2fb9dd2af
Taylor Marvin <Taylor@Marvin.email>
2022-02-19 23:56:15 -0800

    Github actions changes

--- build/BuildSimpleBackup.ps1
+++ build/BuildSimpleBackup.ps1
@@ -4,44 +4,17 @@ param (
 	[string]$SOURCEPATH = "$Env:CLOUDSHARESYNC_SOURCEPATH",
 
 	[Parameter(Mandatory)]
-	[string]$BUILDPATH = "$Env:CLOUDSHARESYNC_BUILDPATH",
-
-	[Parameter(Mandatory)]
 	[string]$PUBLISHPATH = "$Env:CLOUDSHARESYNC_PUBLISHPATH"
 )
 
 $Verbose = $PSBoundParameters.Keys -icontains 'Verbose' ? $PSBoundParameters['Verbose'] : $false
 
-$SystemRoot = $IsLinux ? '/' : (Resolve-Path -Path $env:SystemDrive).Path
-
-if (
-	((Resolve-Path -Path $BUILDPATH -ErrorAction SilentlyContinue).Path -eq $SystemRoot) -or
-	((Resolve-Path -Path $PUBLISHPATH -ErrorAction SilentlyContinue).Path -eq $SystemRoot)
-) { throw 'BUILDPATH and PUBLISHPATH must be somewhere other than the System Root Directory.' }
-
-# Ensure BUILDPATH exists.
-if ((Test-Path -Path $BUILDPATH) -eq $false) {
-	New-Item -Path $BUILDPATH -ItemType Directory -ErrorAction Stop -Verbose:$Verbose 1> $null
-}
-
-# Ensure BUILDPATH/PUBLISHPATH are empty.
-if ((Get-ChildItem -Path $BUILDPATH -ErrorAction SilentlyContinue).Count -gt 0) {
-	throw "BUILDPATH '$BUILDPATH' has existing files."
-}
-if ((Get-ChildItem -Path $PUBLISHPATH -ErrorAction SilentlyContinue).Count -gt 0) {
-	throw "PUBLISHPATH '$PUBLISHPATH' has existing files."
-}
-
-# Copy code to build path.
-Write-Host "Copying source files to '$BUILDPATH'." -ForegroundColor Green
-Copy-Item -Path "$SOURCEPATH/*" -Destination "$BUILDPATH/" -Recurse -Verbose:$Verbose -ErrorAction Stop
-
 # Restore, Build, Publish SimpleBackup.
 Write-Host 'Building and publishing SimpleBackup.' -ForegroundColor Green
-dotnet restore "$BUILDPATH/src/SimpleBackup/Cloud-ShareSync.SimpleBackup.csproj"
-dotnet publish "$BUILDPATH/src/SimpleBackup/Cloud-ShareSync.SimpleBackup.csproj" -c Release -o "$PUBLISHPATH"
+dotnet restore "$SOURCEPATH/src/SimpleBackup/Cloud-ShareSync.SimpleBackup.csproj"
+dotnet publish "$SOURCEPATH/src/SimpleBackup/Cloud-ShareSync.SimpleBackup.csproj" -c Release -o "$PUBLISHPATH"
 
-$AppSettings = Resolve-Path -Path "$BUILDPATH/src/Core/Configuration/appsettings.json"
+$AppSettings = Resolve-Path -Path "$SOURCEPATH/src/Core/Configuration/appsettings.json"
 $DefaultConfigPath = Join-Path -Path $PUBLISHPATH -ChildPath 'Configuration'
 Write-Host "Copying appsettings.json to default config path '$DefaultConfigPath'." -ForegroundColor Green
 if ((Test-Path -Path $DefaultConfigPath) -eq $false) {
